fa5de2b2cb7c593b356101251122d78973044df5
nl1cs <ntna102@gmail.com>
2025-01-21 21:00:56 +0000

    Initial Commit

---
+++ DSAgame/DSAgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSAgameApp.swift
+//  DSAgame
+//
+//  Created by Anh Nguyen on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DSAgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
